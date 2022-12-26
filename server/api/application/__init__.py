717876af6e47de6d9d407a1fcf0a7d6c4ee07467
Kobanoss <mlgspiritofficial@gmail.com>
2022-12-26 23:09:58 +0300

    Removed test data

--- server/api/application/__init__.py
+++ server/api/application/__init__.py
@@ -17,13 +17,3 @@ App.register_blueprint(api, url_prefix='/')
 with App.app_context():
     Db.create_all()
     Db.session.commit()
-
-    Db.session.add(UserContacts(full_name='test test test',
-                                birth_date=123123123,
-                                address='flaisedj;fsjadfas',
-                                phone=79124124124))
-    Db.session.add(UserContacts(full_name='test test1 test1',
-                                birth_date=123123123,
-                                address='flaisedj;fsjadfas',
-                                phone=79124124124))
-    Db.session.commit()
