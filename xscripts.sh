502622831b22cda38e9f48fec9093e7887c60b99
Vladimir Kondrativ <47609905+Kobanoss@users.noreply.github.com>
2023-01-09 16:53:24 +0300

    Create xscripts.sh

---
+++ xscripts.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+chmod +x "start_server.sh"
+chmod +x "stop_server.sh"
+chmod +x "kill_server.sh"
+chmod +x "build_server.sh"
+chmod +x "build_desktop_app.sh"
+chmod +x "start_desktop_app.sh"
+chmod +x "remove_desktop_app.sh"
+chmod +x "server/api/entrypoint.sh"
